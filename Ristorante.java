d4a8eead98f4b1f223bf72347688d1b0a75afef1
Chris9791 <126456770+Chris9791@users.noreply.github.com>
2023-03-01 12:31:23 +0100

    terminato esercizio scelta pietanze
    Non sono state utilizzate (purtroppo) delle classi

--- Ristorante.java
+++ Ristorante.java
@@ -1,6 +1,6 @@
 package giorno3;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Ristorante {
@@ -12,66 +12,91 @@ public class Ristorante {
 				"Carbonara, 10, pasta - uova - pecorino - guanciale",
 				"Pasta al pesto, 8, pasta - pesto"				
 		};
-		ArrayList<String> carrello = new ArrayList<String>();
+		
 		
 		boolean confirm = true;
 		Scanner select = new Scanner(System.in);
-		int start;
+		int start = 3;
 		//variabile scelta ordina o esci
 		int scelta1 = -1;
 		//variabile scelta vedi menu, ordina e esci
 		int scelta2;
 		//variabile scelta ordini
 		int scelta3;
+		//variabile del carrello
+		int carrello = 0;
 		
 //		System.out.println(menu.length);
 		
 		
-		System.out.println("Vuoi ordinare o uscire?\nPremi:\n1) per ordinare\n2) per uscire");
-		start = select.nextInt();
+		System.out.println("Vuoi sederti o uscire dal ristorante?\nPremi:\n1) "
+				+ "per accomodarti\n2) per uscire");
+		
 //		System.out.println(scelta);
 		//dopo aver inserito la scelta con 1 o 2 utilizziamo lo switch per imporre un'altra
 		//scelta all'utente, menu, ordinazione, o uscire dallo switch
-		if (start == 1) {
-
-			while(scelta1 < 1 || scelta1 > 3 ) {
-				System.out.println("Scegli tra le seguenti opzioni:\n1) Vedi menu\'\n2) "
-						+ "Ordina un piatto\n3) Esci");
-				scelta2 = select.nextInt();
-				switch (scelta2) {
-				case 1: //scelta di vedere il menù
-					System.out.println("menu:");
-					for (int i=0; i<menu.length; i++) {
-						System.out.println(menu[i].split(",")[0] + ". Ingredienti: " + menu[i].split(",")[2].trim());
-					}
-					System.out.println("\n");
+		
+		while ((start>2 || start<1) && confirm) {
+			start = select.nextInt();
+			if (start == 1) {
+				
+				while(scelta1 < 1 || scelta1 > 3 ) {
+					System.out.println("Scegli tra le seguenti opzioni:\n1) Vedi menu\'\n2) "
+							+ "Ordina un piatto\n3) Esci e paga");
+					scelta2 = select.nextInt();
+					switch (scelta2) {
+					case 1: //scelta di vedere il menù
+						System.out.println("menu:");
+						for (int i=0; i<menu.length; i++) {
+							System.out.println(menu[i].split(",")[0] + ". Ingredienti: " + 
+									menu[i].split(",")[2].trim());
+						}
+						System.out.println("\n");
 //					scelta1 = scelta2;
-					break;
-				case 2: //scelta per ordinare un piatto
-					System.out.println("ordina selezionando uno dei numeri proposti");
-					for (int i=0; i<menu.length; i++) {
-						System.out.println(i+1 + ") " + menu[i].split(",")[0] + " € " + menu[i].split(",")[1].trim());
-					}
-					System.out.println("\n");
-					scelta3 = select.nextInt();
-					System.out.println("Hai scelto " + menu[scelta3 - 1].split(",")[0]);
-
-					break;
-				case 3: //scelta per uscire
-					System.out.println("esci");
-					scelta1 = scelta2;
-					break;
-				default:
-					System.out.println("Hai inserito un numero errato, riprova.");
+						break;
+					case 2: //scelta per ordinare un piatto
+						System.out.println("ordina selezionando uno dei numeri proposti");
+						for (int i=0; i<menu.length; i++) {
+							System.out.println(i+1 + ") " + menu[i].split(",")[0] + " € " + 
+									menu[i].split(",")[1].trim());
+						}
+						System.out.println("\n");
+						scelta3 = select.nextInt();
+						//impongo una condizione per poter far scegliere solo le pietanze in lista
+						while (scelta3>3 || scelta3<1) {
+							System.out.println("hai inserito prodotti non in lista, "
+									+ "scegli solo quelli presentati.");
+							scelta3 = select.nextInt();
+						}
+						
+						carrello = carrello + Integer.parseInt(menu[scelta3 - 1].split(",")[1].trim());
+//					System.out.println("Attualmente stai spendendo € " + carrello);
+						System.out.println("Hai scelto " + menu[scelta3 - 1].split(",")[0] + " ed è "
+								+ "stato inserito nel pre-conto.\nFai altro?");
+						break;
+					case 3: //scelta per uscire dove viene mostrato il conto
+						System.out.println("Recati in cassa per pagare, il conto è di € " + carrello);
+						
+						scelta1 = scelta2;
+						break;
+					default:
+						System.out.println("Hai inserito un numero errato, riprova.");
 //					scelta = select.nextInt();
+					}
+					
+					
 				}
 				
-				
+			} else if (start == 2) {
+				System.out.println("Sei uscito");
+				select.close();
+				confirm = false;
+			} else {
+				System.out.println("Hai inserito una scelta errata, riprova");
 			}
 			
-		} else if (start == 2) {
-			
 		}
+		
 		
 		
 		
